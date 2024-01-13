49a1acbadf85a676a95d6768077edf9d94cd3172
Isaac-Seungwon <zhzkdkrak@naver.com>
2024-01-13 19:56:43 +0900

    feat(news, event, benefit): 파일 추가

--- src/main/java/com/test/bank/event/controller/UserEventController.java
+++ src/main/java/com/test/bank/event/controller/UserEventController.java
@@ -2,6 +2,8 @@ package com.test.bank.event.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import com.test.bank.event.service.EventService;
 
@@ -10,5 +12,23 @@ public class UserEventController {
 
 	@Autowired
 	private EventService service;
+
+	@GetMapping(value="/news.do")
+	public String news(Model model) {
+		
+		return "user/news/view";
+	}
 	
+	@GetMapping(value="/event.do")
+	public String event(Model model) {
+		
+		return "user/event/view";
+	}
+
+	@GetMapping(value="/benefit.do")
+	public String benefit(Model model) {
+		
+		return "user/benefit/view";
+	}
+
 }
