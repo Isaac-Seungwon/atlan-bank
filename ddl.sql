85ebee5452785a34904b07aedaa3a868c3b927c2
heoSuKyeong <fcb1014@hanyang.ac.kr>
2024-01-19 20:51:50 +0900

    content: 대기표 관련 ddl, dml 추가

--- ddl.sql
+++ ddl.sql
@@ -2,16 +2,44 @@
 create user atlanbank identified by admin1234;
 grant connect, resource, dba to atlanbank;
 
-select * from tabs order by table_name;
-
 -- delete table
+delete from tblBankFavorite;
+delete from tblTicketWaitingStatus;
 delete from tblMember;
+delete from tblWorkDoc;
+delete from tblBankWork;
+delete from tblDetailWork;
+delete from tblDoc;
+delete from tblBank;
+delete from tblWork;
+
+
 
 -- drop table
+drop table tblBankFavorite;
+drop table tblTicketWaitingStatus;
 drop table tblMember;
+drop table tblWorkDoc;
+drop table tblBankWork;
+drop table tblDetailWork;
+drop table tblDoc;
+drop table tblBank;
+drop table tblWork;
+
+
+
 
 -- drop sequence
 drop sequence seqtblMember;
+drop sequence bank_seq;
+drop sequence work_seq;
+drop sequence detail_work_seq;
+drop sequence bank_work_seq;
+drop sequence doc_seq;
+drop sequence work_doc_seq;
+drop sequence bank_favorite_seq;
+drop sequence ticket_waiting_status_seq;
+
 
 -- create table
 create table tblMember (
@@ -30,5 +58,97 @@ create table tblMember (
 	point VARCHAR2(500) not null /* 포인트 */
 );
 
+/* 지점(은행) 테이블 */
+create table tblBank (
+    bank_seq NUMBER primary key, /* 지점번호 */
+    bank_name VARCHAR2(500) not null, /* 지점명 */
+    bank_address VARCHAR2(1000) not null, /* 주소 */
+    lat NUMBER not null, /* 위도 */
+    lng NUMBER not null, /* 경도 */
+    start_date DATE DEFAULT TO_DATE('09:00', 'HH24:MI') not null, /* 영업시작시간*/
+    end_date DATE DEFAULT TO_DATE('16:00', 'HH24:MI') not null, /*영업종료시간*/
+    type NUMBER DEFAULT 1 not null, /* 지점유형(1: 지점, 2: ATM)*/
+    tel VARCHAR2(100) not null /* 전화번호 */
+);
+
+
+/* 업무 테이블 */
+CREATE TABLE tblWork (
+    work_seq NUMBER PRIMARY KEY, /* 업무번호 */
+    work_name VARCHAR(100) not null, /* 업무명 */
+    waiting_time NUMBER DEFAULT 10 not null/* 대기시간 */
+);
+
+/* 세부업무 테이블 */
+CREATE TABLE tblDetailWork (
+    detail_work_seq NUMBER PRIMARY KEY, /* 세부업무번호 */
+    work_seq NUMBER not null, /* 업무번호 */
+    detail_work_name VARCHAR(100) not null, /* 세부업무명 */
+    FOREIGN KEY (work_seq) REFERENCES tblWork(work_seq)
+);
+
+
+/* 은행 별 업무 테이블 */
+CREATE TABLE tblBankWork (
+    bank_work_seq NUMBER PRIMARY KEY, /* 은행별업무번호 */
+    bank_seq NUMBER not null, /* 은행번호 */
+    work_seq NUMBER not null, /* 업무번호 */
+    FOREIGN KEY (bank_seq) REFERENCES tblBank(bank_seq),
+    FOREIGN KEY (work_seq) REFERENCES tblWork(work_seq)
+);
+
+
+/* 서류 테이블 */
+CREATE TABLE tblDoc (
+    doc_seq NUMBER PRIMARY KEY, /* 서류번호 */
+    doc_name VARCHAR(200) not null /* 서류명 */
+);
+
+
+/* 업무 별 서류 테이블 */
+CREATE TABLE tblWorkDoc (
+    work_doc_seq NUMBER PRIMARY KEY, /* 업무별서류번호 */
+    detail_work_seq NUMBER not null, /* 세부업무번호 */
+    doc_seq NUMBER not null, /* 서류번호 */
+    FOREIGN KEY (detail_work_seq) REFERENCES tblDetailWork(detail_work_seq),
+    FOREIGN KEY (doc_seq) REFERENCES tblDoc(doc_seq)
+);
+
+/* 지점(은행) 즐겨찾기 테이블 */
+CREATE TABLE tblBankFavorite (
+    bank_favorite_seq NUMBER PRIMARY KEY, /* 지점즐겨찾기번호 */
+    bank_seq NUMBER not null, /* 지점 번호 */
+    member_seq NUMBER not null, /* 회원 번호 */
+    FOREIGN KEY (bank_seq) REFERENCES tblBank(bank_seq),
+    FOREIGN KEY (member_seq) REFERENCES tblMember(member_seq)
+);
+
+/* 대기리스트 테이블 */
+CREATE TABLE tblTicketWaitingStatus (
+    ticket_waiting_status_seq NUMBER PRIMARY KEY, /* 대기리스트 번호 */
+    bank_seq NUMBER not null, /* 지점 번호 */
+    detail_work_seq NUMBER not null, /* 세부업무 번호 */
+    member_seq NUMBER not null, /* 회원 번호 */
+    regdate	DATE DEFAULT SYSDATE, /* 신청일자 */
+    time DATE not null, /* 신청시간 */ 
+    is_complete NUMBER DEFAULT 0 not null, /* 완료여부(1: 완료, 0: 대기) */ 
+    FOREIGN KEY (bank_seq) REFERENCES tblBank(bank_seq),
+    FOREIGN KEY (detail_work_seq) REFERENCES tblDetailWork(detail_work_seq),
+    FOREIGN KEY (member_seq) REFERENCES tblMember(member_seq)
+
+);
+
+
+
 -- creat sequence
 create sequence seqtblMember;
+create sequence bank_seq;
+create sequence work_seq;
+create sequence detail_work_seq;
+create sequence bank_work_seq;
+create sequence doc_seq;
+create sequence work_doc_seq;
+create sequence bank_favorite_seq;
+create sequence ticket_waiting_status_seq;
+
+
