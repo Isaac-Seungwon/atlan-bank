b707964ba4925a4720b1d2c63be65c5202884a36
dmsalsgus <leeje971118@naver.com>
2024-01-23 14:54:10 +0900

    controller 수정

--- src/main/java/com/test/bank/forex/controller/UserForexController.java
+++ src/main/java/com/test/bank/forex/controller/UserForexController.java
@@ -6,15 +6,17 @@ import org.springframework.ui.Model;
 
 import com.test.bank.forex.service.ForexService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
+@RequestMapping(value = "/user/forex")
 public class UserForexController {
 
 	@Autowired
 	private ForexService service;
 	
-	@GetMapping(value="/forex.do")
+	@GetMapping(value="/view.do")
 	public String forex(Model model) {
 		
 		return "user/forex/view";
