5e6fcdc77be9f8783da6b64d73866963d65c0ce8
dmsalsgus <leeje971118@naver.com>
2024-01-30 20:14:21 +0900

    feat: api dependency

--- src/main/java/com/test/bank/forex/controller/UserForexController.java
+++ src/main/java/com/test/bank/forex/controller/UserForexController.java
@@ -1,23 +1,42 @@
 package com.test.bank.forex.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 
+import com.test.bank.forex.domain.ForexDTO;
 import com.test.bank.forex.service.ForexService;
+
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
-@RequestMapping(value = "atlanbank/user/forex")
+@RequestMapping(value = "/user/forex")
 public class UserForexController {
 
 	@Autowired
 	private ForexService service;
 	
+	@ApiOperation(value="환율 API 동작여부 확인", notes="한국 수출입 은행 api 호출, 11시 이전 혹은 1000번 이상 호출 시 Null")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "API 정상 작동"),
+            @ApiResponse(code = 500, message = "서버 에러")
+    })
+	
 	@GetMapping(value="/view.do")
 	public String view(Model model) {
+		
+//		ForexDTO[] dto = service.getExchangeRate();
+//        HttpHeaders httpHeaders = new HttpHeaders();
+//        httpHeaders.setContentType(new MediaType("application", "json", Charset.forName("UTF-8")));
+//
+//        model.addAttribute(dto, httpHeaders, HttpStatus.OK);
 		
 		return "user/forex/view";
 	}
--- src/main/java/com/test/bank/forex/domain/ForexDTO.java
+++ src/main/java/com/test/bank/forex/domain/ForexDTO.java
@@ -1,8 +1,22 @@
 package com.test.bank.forex.domain;
 
+import java.time.LocalDateTime;
+
 import lombok.Data;
 
 @Data
-public class ForexDTO {
-
+public class ForexDTO {  //환율정보
+		int result;  //조회 결과	1 : 성공, 2 : DATA코드 오류, 3 : 인증코드 오류, 4 : 일일제한횟수 마감
+		String cur_unit; //통화코드
+	    String cur_nm;  //국가/통화명
+	    String ttb;  //전신환-받으실때
+	    String tts;	  //전신환-보내실때
+	    String deal_bas_r;  //매매기준율
+	    String bkpr;  //장부가격
+	    String yy_efee_r;  //년환가료율
+	    String ten_dd_efee_r;  //10일환가료율
+	    String kftc_deal_bas_r;  //서울외국환중개 매매기준율
+	    String kftc_bkpr;  //서울외국환중개 장부가격
+	    //LocalDateTime exchangeDate;  //API 호출시간
+	
 }
