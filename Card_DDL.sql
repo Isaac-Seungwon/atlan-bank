b0743415cac3b99305a13fb5be07a3479a663d05
NaraeP <skfocjstk93@naver.com>
2024-01-31 15:48:22 +0900

    content: 카드 DDL, DML 수정

--- Card_DDL.sql
+++ Card_DDL.sql
@@ -1,17 +1,19 @@
 --[SEQUENCE 삭제]
-DROP SEQUENCE card_benefit_seq;
-DROP SEQUENCE payment_seq;
-DROP SEQUENCE member_card_history_seq;
-DROP SEQUENCE member_card_seq;
 DROP SEQUENCE card_seq;
-DROP SEQUENCE performance_benefit_seq;
+DROP SEQUENCE card_usage_guide_seq;
 DROP SEQUENCE benefits_seq;
+DROP SEQUENCE card_benefit_seq;
+DROP SEQUENCE performance_benefit_seq;
+--DROP SEQUENCE member_card_seq;
+DROP SEQUENCE member_card_history_seq;
+DROP SEQUENCE payment_seq;
 
 --[TABLE 내용 삭제]
 DELETE FROM tblCardBenefit;
 DELETE FROM tblPayment;
 DELETE FROM tblMemberCardHistory;
 DELETE FROM tblMemberCard;
+DELETE FROM tblCardUsageGuide;
 DELETE FROM tblCard;
 DELETE FROM tblPerformanceBenefit;
 DELETE FROM tblBenefits;
@@ -21,18 +23,20 @@ DROP TABLE tblCardBenefit;
 DROP TABLE tblPayment;
 DROP TABLE tblMemberCardHistory;
 DROP TABLE tblMemberCard;
+DROP TABLE tblCardUsageGuide;
 DROP TABLE tblCard;
 DROP TABLE tblPerformanceBenefit;
 DROP TABLE tblBenefits;
 
 --[SEQUENCE 생성]
-CREATE SEQUENCE card_benefit_seq;
-CREATE SEQUENCE payment_seq;
-CREATE SEQUENCE member_card_history_seq;
-CREATE SEQUENCE member_card_seq;
 CREATE SEQUENCE card_seq;
-CREATE SEQUENCE performance_benefit_seq;
+CREATE SEQUENCE card_usage_guide_seq;
 CREATE SEQUENCE benefits_seq;
+CREATE SEQUENCE card_benefit_seq;
+CREATE SEQUENCE performance_benefit_seq;
+--CREATE SEQUENCE member_card_seq;
+CREATE SEQUENCE member_card_history_seq;
+CREATE SEQUENCE payment_seq;
 
 --[TABLE 생성]
 /* 카드 */
@@ -40,16 +44,25 @@ CREATE TABLE tblCard (
 	card_seq NUMBER PRIMARY KEY, /* 카드종류번호 */
 	type VARCHAR2(50) NOT NULL, /* 카드종류 */
 	name VARCHAR2(200) NOT NULL, /* 카드명 */
-    brand VARCHAR2(100) NOT NULL, /* 국내외 사용가능 여부 */
+    brand VARCHAR2(100) NOT NULL, /* 브랜드(국내외 사용가능 여부) */
     annual_fee NUMBER NOT NULL, /* 연회비 */
-    front_img VARCHAR2(200) NOT NULL, /* 카드 앞면 이미지 */
-    back_img VARCHAR2(200) NOT NULL /* 카드 뒷면 이미지 */
+    img VARCHAR2(200) NOT NULL, /* 카드 이미지 */
+    orientation NUMBER DEFAULT 0 NOT NULL /* 이미지방향(0: horizontal, 1: vertical) */
 );
 
-/* 혜택 */
+/* 카드안내사항 */
+CREATE TABLE tblCardUsageGuide (
+	card_usage_guide_seq NUMBER PRIMARY KEY, /* 카드안내사항번호 */
+	card_seq NUMBER REFERENCES tblCard(card_seq) NOT NULL, /* 카드종류번호 */
+	type VARCHAR2(200) NOT NULL, /* 안내사항종류 */
+    content VARCHAR2(2000) NOT NULL /* 상세내용 */
+);
+
+/* 카드혜택 */
 CREATE TABLE tblBenefits (
     benefits_seq NUMBER PRIMARY KEY, /* 혜택번호 */
-    type VARCHAR2(50) NOT NULL, /* 혜택종류 */
+    type NUMBER NOT NULL, /* 혜택종류(1: 할인, 2: 적립, 3: 항공마일리지적립) */
+    category VARCHAR2(200) NOT NULL, /* 혜택항목 */
     subject VARCHAR2(200) NOT NULL, /* 혜택명 */
     content VARCHAR2(2000) NOT NULL /* 혜택 상세 */
 );
@@ -66,13 +79,13 @@ CREATE TABLE tblPerformanceBenefit (
 	performance_benefit_seq NUMBER PRIMARY KEY, /* 카드실적별혜택번호 */
 	benefits_seq NUMBER REFERENCES tblBenefits(benefits_seq) NOT NULL, /* 혜택번호 */
 	prev_month_perf NUMBER NOT NULL, /* 전월실적(실적별 구분 30/50/100) */
-	rate NUMBER NOT NULL, /* 할인율 */
-	limit NUMBER NOT NULL /* 월 할인한도 */
+	rate NUMBER NOT NULL, /* 할인/적립율 */
+	limit NUMBER NOT NULL /* 월 할인/적립 한도 */
 );
 
 /* 회원카드 */
 CREATE TABLE tblMemberCard (
-	member_card_seq NUMBER PRIMARY KEY, /* 회원카드번호 */
+	member_card_no NUMBER PRIMARY KEY, /* 회원카드번호(16자리) */
 	member_seq NUMBER REFERENCES tblMember(member_seq) NOT NULL, /* 회원번호 */
 	card_seq NUMBER REFERENCES tblCard(card_seq) NOT NULL, /* 카드종류번호 */
 	exp DATE NOT NULL, /* 만료일 */
@@ -84,7 +97,7 @@ CREATE TABLE tblMemberCard (
 /* 회원카드이용내역 */
 CREATE TABLE tblMemberCardHistory (
 	member_card_history_seq NUMBER PRIMARY KEY, /* 회원카드이용내역번호 */
-	member_card_seq NUMBER REFERENCES tblMemberCard(member_card_seq) NOT NULL, /* 회원카드번호 */
+	member_card_no NUMBER REFERENCES tblMemberCard(member_card_no) NOT NULL, /* 회원카드번호 */
 	transaction_date DATE NOT NULL, /* 결제일 */
 	amount NUMBER NOT NULL, /* 금액 */
 	installment_months NUMBER NOT NULL /* 할부 개월수 */
--- Card_DML.sql
+++ Card_DML.sql
@@ -3,14 +3,20 @@ commit;
 select * from tblCard;
 
 -- tblCard
-INSERT INTO tblCard (card_seq, type, name, brand, annual_fee, front_img, back_img) VALUES (card_seq.nextVal, '신용카드', '아틀란카드', '국내전용', 5000, 'front.png', 'back.png');
-INSERT INTO tblCard (card_seq, type, name, brand, annual_fee, front_img, back_img) VALUES (card_seq.nextVal, '신용카드', '아틀란카드', 'VISA', 15000, 'front.png', 'back.png');
-INSERT INTO tblCard (card_seq, type, name, brand, annual_fee, front_img, back_img) VALUES (card_seq.nextVal, '신용카드', '당근카드', '국내전용', 5000, 'front.png', 'back.png');
-INSERT INTO tblCard (card_seq, type, name, brand, annual_fee, front_img, back_img) VALUES (card_seq.nextVal, '신용카드', '당근카드', 'Master', 15000, 'front.png', 'back.png');
+INSERT INTO tblCard (card_seq, type, name, brand, annual_fee, img, orientation) VALUES (card_seq.nextVal, '신용카드', '아틀란카드', '국내전용', 5000, 'front.png', default);
+INSERT INTO tblCard (card_seq, type, name, brand, annual_fee, img, orientation) VALUES (card_seq.nextVal, '신용카드', '아틀란카드', 'VISA', 15000, 'front.png', default);
+INSERT INTO tblCard (card_seq, type, name, brand, annual_fee, img, orientation) VALUES (card_seq.nextVal, '신용카드', '당근카드', '국내전용', 5000, 'front.png', default);
+INSERT INTO tblCard (card_seq, type, name, brand, annual_fee, img, orientation) VALUES (card_seq.nextVal, '신용카드', '당근카드', 'Master', 15000, 'front.png', default);
+
+--tblCardUsageGuide
+INSERT INTO tblCardUsageGuide (card_usage_guide_seq, card_seq, type, content) VALUES (card_usage_guide_seq.nextVal, 1, '전월 이용실적 기준', '전월 1일 ~ 말일까지 KB국민 알뜰교통플러스카드의 일시불 및 할부 승인 금액 기준
+해외 이용금액은 전월 1일 ~ 말일까지 일시불 매입 완료 기준
+취소 금액은 취소 전표가 당사에 접수된 월의 실적에서 차감');
+
 
 -- tblBenefits
-INSERT INTO tblBenefits (benefits_seq, type, subject, content) VALUES (benefits_seq.nextVal, '여행/항공', 'Agoda 할인', '전세계 호텔 예약이 가능한 Agoda에서 호텔 예약 시 청구 할인 제공');
-INSERT INTO tblBenefits (benefits_seq, type, subject, content) VALUES (benefits_seq.nextVal, '여행/항공', '대한항공 마일리지 적립', '1,000원당 3마일리지 적립');
+INSERT INTO tblBenefits (benefits_seq, type, category, subject, content) VALUES (benefits_seq.nextVal, 1, '여행/항공', 'Agoda 할인', '전세계 호텔 예약이 가능한 Agoda에서 호텔 예약 시 청구 할인 제공');
+INSERT INTO tblBenefits (benefits_seq, type, category, subject, content) VALUES (benefits_seq.nextVal, 3, '여행/항공', '대한항공 마일리지 적립', '1,000원당 3마일리지 적립');
 
 -- tblCardBenefit
 INSERT INTO tblCardBenefit (card_benefit_seq, card_seq, benefits_seq) VALUES (card_benefit_seq.nextVal, 2, 2);
