6d5d5d504e8c5b31952d61fc535d2b0d86f32d08
dmsalsgus <leeje971118@naver.com>
2024-02-05 15:15:24 +0900

    feat : api가져오기 완료

--- src/main/java/com/test/bank/forex/controller/UserForexController.java
+++ src/main/java/com/test/bank/forex/controller/UserForexController.java
@@ -32,61 +32,60 @@ public class UserForexController {
 	@Autowired
 	private ForexService service;
 	
-	public List<ForexDTO> exchangeList() {  //페이지에서 환율 정보 가져오기
-
-		String url = "https://finance.naver.com/marketindex/exchangeList.naver";
-		Document doc = null;
-
-		List<ForexDTO> exchangeList = new ArrayList<>();
-
-		try {
-
-			doc = Jsoup.connect(url).get(); 
-
-		} catch (IOException e) {
-
-			e.printStackTrace();
-
-		}
-
-		Elements element1 = doc.select("table tbody");
-		Elements element2 = element1.select("tr");
-
-		String[] a = null;
-
-		for (Element element : element2) {
-			int i = 0;
-			a = element.text().split(" ");
-			for (int j = 0; j < a.length; j++) {
-				if (a[j].equals("N/A")) {
-					a[j] = "0";
-				}
-			}
-			ForexDTO dto = new ForexDTO();
-			if (a[2].equals("(100엔)") || a[2].equals("ZAR")) {
-				dto.setNationKr(a[i++] + a[i++] + a[i++]);
-			} else {
-				dto.setNationKr(a[i++] + a[i++]);
-			}
-			dto.setBuyBasicRate(Float.parseFloat(a[i++].replace(",", "")));
-			dto.setCashBuyRate(Float.parseFloat(a[i++].replace(",", "")));
-			dto.setCashSellRate(Float.parseFloat(a[i++].replace(",", "")));
-			dto.setTransferSendRate(Float.parseFloat(a[i++].replace(",", "")));
-			dto.setTransferReceiveRate(Float.parseFloat(a[i++].replace(",", "")));
-			dto.setUsdChangeRate(Float.parseFloat(a[i].replace(",", "")));
-			exchangeList.add(dto);
-
-		}
-
-		return exchangeList;
-
-	}
+//	public List<ForexDTO> exchangeList() {  //페이지에서 환율 정보 가져오기
+//
+//		String url = "https://finance.naver.com/marketindex/exchangeList.naver";
+//		Document doc = null;
+//
+//		List<ForexDTO> exchangeList = new ArrayList<>();
+//
+//		try {
+//
+//			doc = Jsoup.connect(url).get(); 
+//
+//		} catch (IOException e) {
+//
+//			e.printStackTrace();
+//
+//		}
+//
+//		Elements element1 = doc.select("table tbody");
+//		Elements element2 = element1.select("tr");
+//
+//		String[] a = null;
+//
+//		for (Element element : element2) {
+//			int i = 0;
+//			a = element.text().split(" ");
+//			for (int j = 0; j < a.length; j++) {
+//				if (a[j].equals("N/A")) {
+//					a[j] = "0";
+//				}
+//			}
+//			ForexDTO dto = new ForexDTO();
+//			if (a[2].equals("(100엔)") || a[2].equals("ZAR")) {
+//				dto.setNationKr(a[i++] + a[i++] + a[i++]);
+//			} else {
+//				dto.setNationKr(a[i++] + a[i++]);
+//			}
+//			dto.setBuyBasicRate(Float.parseFloat(a[i++].replace(",", "")));
+//			dto.setCashBuyRate(Float.parseFloat(a[i++].replace(",", "")));
+//			dto.setCashSellRate(Float.parseFloat(a[i++].replace(",", "")));
+//			dto.setTransferSendRate(Float.parseFloat(a[i++].replace(",", "")));
+//			dto.setTransferReceiveRate(Float.parseFloat(a[i++].replace(",", "")));
+//			dto.setUsdChangeRate(Float.parseFloat(a[i].replace(",", "")));
+//			exchangeList.add(dto);
+//
+//		}
+//
+//		return exchangeList;
+//
+//	}
 	
 	@GetMapping(value="/view.do")
 	public String view(Model model, HttpServletRequest request) {
 		
-		//request.setAttribute("exchangelist", exchangeList());
-		model.addAttribute("exchangelist", exchangeList());
+		model.addAttribute("exchangelist", service.exchangeList());
 		
 		return "user/forex/view";
 	}
