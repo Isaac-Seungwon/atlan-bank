3c23ea7d4122ed2ac4efeb6d0e332e8ea12602e8
Isaac-Seungwon <zhzkdkrak@naver.com>
2024-02-28 14:33:52 +0900

    Feat: 혜택 페이지 구현 헤택 데이터와 가맹점 데이터를 통합하여 구현

--- src/main/java/com/test/bank/benefit/controller/UserBenefitController.java
+++ src/main/java/com/test/bank/benefit/controller/UserBenefitController.java
@@ -1,11 +1,14 @@
 package com.test.bank.benefit.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.test.bank.benefit.domain.BenefitDTO;
 import com.test.bank.benefit.service.BenefitService;
 
 @Controller
@@ -18,6 +21,10 @@ public class UserBenefitController {
 	@GetMapping(value = "/view.do")
 	public String view(Model model) {
 
+		List<BenefitDTO> benefitList = service.getBenefits();
+		
+		model.addAttribute("benefitList", benefitList);
+		
 		return "user/benefit/view";
 	}
 
--- src/main/java/com/test/bank/benefit/repository/BenefitDAO.java
+++ src/main/java/com/test/bank/benefit/repository/BenefitDAO.java
@@ -1,5 +1,11 @@
 package com.test.bank.benefit.repository;
 
+import java.util.List;
+
+import com.test.bank.benefit.domain.BenefitDTO;
+
 public interface BenefitDAO {
+
+	List<BenefitDTO> getBenefits();
 	
 }
