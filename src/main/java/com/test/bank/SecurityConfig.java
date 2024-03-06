86629dadf9b5569bfad7c7e1dd31ea5db087fe7d
pengzer1 <pengzer1@gmail.com>
2024-03-06 21:20:05 +0900

    feat. 로그인 바로 직전

--- src/main/java/com/test/bank/SecurityConfig.java
+++ src/main/java/com/test/bank/SecurityConfig.java
@@ -1,17 +1,34 @@
 package com.test.bank;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
 
+import com.test.bank.login.service.UserSecurityService;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+	
+	@Autowired
+	private UserSecurityService userDetailsService;
+
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+
+	@Autowired
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService);
+	}
+
 	@Bean
 	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http.csrf((csrfConfig) -> csrfConfig.disable())
@@ -20,17 +37,17 @@ public class SecurityConfig {
 				.authorizeHttpRequests((authorizeRequests) -> authorizeRequests
 						/* .requestMatchers("/user/**").hasRole("USER") */
 						.requestMatchers("/**").permitAll().anyRequest().authenticated())
-				.headers((headers) -> headers.addHeaderWriter(
-						new XFrameOptionsHeaderWriter(XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN)))
-				.formLogin((formLogin) -> formLogin.loginPage("/login").usernameParameter("id").passwordParameter("pw")
-						.loginProcessingUrl("/login/login-proc").defaultSuccessUrl("/index.do", true))
+//				.csrf((csrf) -> csrf.ignoringRequestMatchers(new AntPathRequestMatcher("/h2-console/**")))
+				.csrf().disable()
+				/*
+				 * .formLogin((formLogin) ->
+				 * formLogin.loginPage("/login").usernameParameter("id").passwordParameter("pw")
+				 * .loginProcessingUrl("/login/login-proc").defaultSuccessUrl("/index.do",
+				 * true))
+				 */
+				.formLogin((formLogin) -> formLogin.loginPage("/user/login").defaultSuccessUrl("/index.do"))
 				.logout((logoutConfig) -> logoutConfig.logoutSuccessUrl("/index.do"));
 
 		return http.build();
-	}
-	
-	@Bean
-	PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
 	}
 }
---
+++ src/main/java/com/test/bank/login/service/UserSecurityService.java
@@ -0,0 +1,32 @@
+package com.test.bank.login.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.test.bank.login.domain.CustomUser;
+import com.test.bank.login.mapper.LoginMapper;
+import com.test.bank.member.domain.MemberDTO;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class UserSecurityService implements UserDetailsService {
+	
+	@Autowired
+	private LoginMapper mapper;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		System.out.println("id: " + username);
+		MemberDTO dto = mapper.read(username);
+		
+		System.out.println(dto);
+		
+		return dto != null ? new CustomUser(dto) : null;
+		
+	}
+}
