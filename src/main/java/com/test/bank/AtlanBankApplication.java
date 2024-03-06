eb46e2fa3f1d1b78b1ca1acd13fa079e9a254e3f
Isaac-Seungwon <zhzkdkrak@naver.com>
2024-03-06 21:05:45 +0900

    chore: 배포 형식 지정

--- src/main/java/com/test/bank/AtlanBankApplication.java
+++ src/main/java/com/test/bank/AtlanBankApplication.java
@@ -7,9 +7,9 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 @MapperScan(basePackages = { "com.test.bank.event.mapper", "com.test.bank.benefit.mapper", "com.test.bank.news.mapper", "com.test.bank.member.mapper", "com.test.bank.ticket.mapper", "com.test.bank.card.mapper", "com.test.bank.forex.mapper", "com.test.bank.loan.mapper", "com.test.bank.deposit.mapper" })
-	public class AtlanBankApplication extends SpringBootServletInitializer {
+public class AtlanBankApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AtlanBankApplication.class, args);
