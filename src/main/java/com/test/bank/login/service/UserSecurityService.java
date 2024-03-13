1fd5cb0280bc3ce03b48065a1c1240901221a24e
pengzer1 <pengzer1@gmail.com>
2024-03-13 21:19:40 +0900

    feat. Login

--- src/main/java/com/test/bank/login/service/UserSecurityService.java
+++ src/main/java/com/test/bank/login/service/UserSecurityService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.test.bank.login.domain.CustomUser;
@@ -18,6 +19,9 @@ public class UserSecurityService implements UserDetailsService {
 	
 	@Autowired
 	private LoginMapper mapper;
+	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
